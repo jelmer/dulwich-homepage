ee353517953e8969a206aa1330b1b41a01149aef
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-04-27 18:52:46 +0100

    Basic homepage

---
+++ pelicanconf.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Jelmer Vernooij'
+SITENAME = 'Dulwich'
+SITEURL = ''
+
+PATH = 'content'
+
+THEME = 'pelican-themes/iris'
+
+TIMEZONE = 'Etc/UTC'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = (('github', 'https://github.com/dulwich/dulwich'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+PLUGIN_PATHS = ['plugins']
+PLUGINS = ['sitemap', 'assets']
+SITEMAP = {'format': 'xml'}
+
+# Menu
+DISPLAY_CATEGORIES_ON_MENU = False
+DISPLAY_PAGES_ON_MENU = False
+
+INDEX_SAVE_AS = 'news.html'
